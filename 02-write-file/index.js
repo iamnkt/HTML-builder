29dcae273c70c7c35982580503f2111de2f7ce4d
acielle <sg23mj@gmail.com>
2022-11-06 20:27:39 +0300

    feat: add task 02 solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const { stdin: input, stdout } = process;
+
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+const rl = readline.createInterface({input, output});
+stdout.write('Введите текст:\n');
+rl.on('SIGINT', () => {
+  rl.close();
+});
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    return rl.close();
+  }
+  output.write(`${input}\n`);
+});
+process.on('exit', () => stdout.write('Ввод завершен!'));
