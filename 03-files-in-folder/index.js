94fd4f8c8521af3297f99f5f439a3ea746055839
acielle <sg23mj@gmail.com>
2022-11-07 01:49:36 +0300

    feat: add task 03 solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const { readdir } = require('fs');
+
+const actualPath = path.join(__dirname, 'secret-folder');
+console.log(actualPath);
+
+async function showInf() {
+  try {
+    const files = await fs.promises.readdir(actualPath);
+    for (const file of files) {
+      const attr = await fs.promises.stat(path.join(actualPath, file));
+      if (attr.isFile()) {
+        const ext = path.extname(path.join(actualPath, file)).slice(1);
+        console.log(`${file.split('.')[0]} - ${ext} - ${attr.size}b`);
+      }
+    }
+  } catch (err) {
+      console.error(err);
+    }
+};
+
+showInf();
