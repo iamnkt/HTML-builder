e8544fd925719904e0441ad6f921429f7abad034
acielle <sg23mj@gmail.com>
2022-11-09 19:02:06 +0300

    fix: task 02 completed correctly

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,13 +6,20 @@ const { stdin: input, stdout } = process;
 const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 const rl = readline.createInterface({input, output});
 stdout.write('please, type the text:\n');
-rl.on('SIGINT', () => {
-  rl.close();
-});
 rl.on('line', (input) => {
   if (input === 'exit') {
+    console.log('completed!')
     return rl.close();
   }
   output.write(`${input}\n`);
 });
-process.on('exit', () => stdout.write('completed!'));
+
+rl.on('SIGINT', () => {
+  process.emit('SIGINT')
+  rl.close();
+});
+
+process.on('SIGINT', () => {
+  console.log('completed!')
+  process.exit();
+});
